a93d62264411149f139d293a979ae53dfa2675a7
Mahraib Fatima <122329300+MahraibFatima@users.noreply.github.com>
2024-08-08 20:42:11 +0000

    data transformation stage added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.mlProject import logger
 from src.mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from src.mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 
 # from /workspaces/rudder/src/mlProject/pipeline/stage_01_data_ingestion.py
@@ -26,3 +27,14 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+
+#from /workspaces/rudder/src/mlProject/pipeline/stage_03_data_transformation.py
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
--- src/mlProject/entity/config_entity.py
+++ src/mlProject/entity/config_entity.py
@@ -1,9 +1,7 @@
-# from -> /workspaces/rudder/research/01_data_ingestion.ipynb __cell-5__
-
 from dataclasses import dataclass
 from pathlib import Path
 
-# stage_01_DataIngestionConfig
+# from ->  /workspaces/rudder/research/01_data_ingestion.ipynb __cell-5__
 @dataclass(frozen=True)
 class DataIngestionConfig:
     root_dir: Path
@@ -11,10 +9,16 @@ class DataIngestionConfig:
     local_data_file: Path
     unzip_dir: Path
 
-# stage_02_DataValidationConfig
+# from -> /workspaces/rudder/research/02_data_validation.ipynb __cell-5__
 @dataclass(frozen=True)
 class DataValidationConfig:
     root_dir: Path
     STATUS_FILE: str
     unzip_data_dir: Path
     all_schema: dict
+
+# from -> /workspaces/rudder/research/03_data_transformation.ipynb __cell-5__
+@dataclass(frozen=True)
+class DataTransformationConfig:
+    root_dir: Path
+    data_path: Path
